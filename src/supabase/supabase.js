524f900e86db3d0c3b57bc3f072812c67e484ec3
Ethem Serinoglu <132064533+eserinoglu@users.noreply.github.com>
2024-01-31 11:36:36 +0300

    Update supabase.js

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -2,9 +2,9 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ihmsucjyfbgippgjcfnp.supabase.co";
+const supabaseUrl = "YOUR_SUPABASE_URL";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlobXN1Y2p5ZmJnaXBwZ2pjZm5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyMTQwMTcsImV4cCI6MjAxNzc5MDAxN30.bno56hEiQbC7GsY3hqqKS2P9qELbElnaPMFNGcHyRLo";
+  "YOUR_SUPABASE_KEY";
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
